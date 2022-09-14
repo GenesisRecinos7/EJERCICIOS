142a0b58b696a3941dda7f8fb12c909653bde8d9
GenesisRecinos7 <113471026+GenesisRecinos7@users.noreply.github.com>
2022-09-13 22:11:20 -0600

    Add files via upload

---
+++ periodo 1 semana 3 .java
@@ -0,0 +1,12 @@
+package operados; 
+import java.util.Scanner;
+public class Operadores {
+  public static void main (String[] args) {
+    double raiz =Math.sqrt(9);
+
+      
+    System.out.prinntln(raiz);
+  }
+
+}
+    
